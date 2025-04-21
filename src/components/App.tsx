c2820355855cec3350cfb8f77af1517d39929245
BruceLuo <295448382@qq.com>
2025-04-21 17:55:13 +0800

    feat: some of the jobItem show and search

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,45 @@
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
+import BookmarksButton from "./BookmarksButton";
+import Logo from "./Logo";
+import SearchForm from "./SearchForm";
+import { useState } from "react";
+import JobItemContent from "./JobItemContent";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobList from "./JobList";
+import Pagination from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+import { JobItem } from "../lib/types";
+
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   return (
     <>
       <Background />
-      <Header />
-      <Container />
+      <Header>
+        <HeaderTop>
+          <Logo />
+          <BookmarksButton />
+        </HeaderTop>
+
+        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+      </Header>
+      <Container>
+        <Sidebar>
+          <SidebarTop>
+            <ResultsCount />
+            <SortingControls />
+          </SidebarTop>
+
+          <JobList jobItems={jobItems} />
+          <Pagination />
+        </Sidebar>
+        <JobItemContent />
+      </Container>
       <Footer />
     </>
   );
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,27 @@
-export default function SearchForm() {
+export default function SearchForm({
+  searchText,
+  setSearchText,
+}: {
+  searchText: string;
+  setSearchText: (text: string) => void;
+}) {
   return (
-    <form action="#" className="search">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      action="#"
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
         spellCheck="false"
         type="text"
         required
